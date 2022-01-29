602c57ba4ec8cf30efc7acc49740c4fcefc36169
sabinagio <sabinagiorgiana@gmail.com>
2022-01-29 18:00:20 +0200

    Add SQL lab

---
+++ lab-intro-to-sql.sql
@@ -0,0 +1,7 @@
+SELECT * FROM actor, film, customer;
+SELECT titles FROM film_text;
+SELECT DISTINCT name AS languages FROM language;
+SELECT COUNT(store_id) FROM store;
+SELECT COUNT(staff_id) FROM staff;
+SELECT first_name FROM staff;
+
